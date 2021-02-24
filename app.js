6af6e4d79699f11c322ab34b174430dd2a14efd8
Jacob Lamb <jacob.lamb@ada.ac.uk>
2021-02-24 17:48:38 +0000

    made input box show in console

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+console.log("Test")
 const form = document.getElementById('form');
 function getNumber(event) {
+    event.preventDefault();
     console.log("HERE")
 }
 form.addEventListener('submit', getNumber);
